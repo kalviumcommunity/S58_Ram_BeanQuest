72b47518a2a7557633beaab378049f8f0b81d82d
Ram nathawat <nathawatr7@gmail.com>
2024-07-05 17:31:24 +0530

    Post Api working

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const coffeeRoutes = require('./routes/coffee_routes');
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+const uri = process.env.MONGO_URI;
 
 app.use(express.json());
 
@@ -11,16 +14,17 @@ app.get('/', (req, res) => {
   res.send('Welcome to BrewVista!');
 });
 
-const coffeeRoutes = require('./routes/coffee_routes');
 app.use('/api', coffeeRoutes);
 
-const uri = process.env.MONGO_URI;
-mongoose.connect(uri, {})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-  });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
